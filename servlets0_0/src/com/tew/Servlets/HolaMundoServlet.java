6dca7121944d34230c4365405647f990df55e90d
uo263748 <uo263748@uniovi.es>
2020-09-23 18:15:01 +0200

    TEW-2021-102-2.4-Encadenamiento de Servelts

--- servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Vector;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,38 +31,15 @@ public class HolaMundoServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
     public void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-    	PrintWriter out = response.getWriter();
+    	RequestDispatcher dispatcher =getServletContext().getNamedDispatcher("HolaMundoVista");
+		
     	
-    	String nombre = (String) request.getParameter("NombreUsuario");
-    	Vector<String> listado = (Vector<String>)request.getSession().getAttribute("listado");
-    	Integer contador= (Integer) getServletContext().getAttribute("contador");
     	
-    	if (listado == null){listado = new Vector();}
-		if (nombre != null ){out.println("<br>Hola "+nombre+"<br>");listado.addElement(nombre);}
-		if ( contador == null ){contador = new Integer(1);}
     	
-    	request.getSession().setAttribute("listado",listado);
-    	getServletContext().setAttribute("contador",new Integer(contador.intValue()+1));
     	
-    	response.setCharacterEncoding("UTF-8");
-		response.setContentType("text/html");
-		
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-		out.println("<BODY>");
-		
-		out.println("Bienvenido a mi primera página web!!!!!");
-		out.println("<br>");
-		out.println("Contigo, hoy me han visitado:<br>");
-		for ( int i = 0 ; i < listado.size() ; i++ ){
-			out.println("<br>"+(String)listado.elementAt(i));
-		}
-		out.println("<br><br>" + contador +" visitas<br>");
-		out.println("<a href=\"index.html\">volver</a>");
-		
-		
-		out.println("</BODY></HTML>");
-		
+    	
+    	
+    	dispatcher.forward(request, response);
 
 	}
 
