47628d25fff2784bb73a104b51b67a5faacd1c86
uo263748 <uo263748@uniovi.es>
2020-09-23 17:57:44 +0200

    TEW-2021-102-2.3-Servelts con contexto

--- servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++ servlets0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -34,17 +34,14 @@ public class HolaMundoServlet extends HttpServlet {
     	
     	String nombre = (String) request.getParameter("NombreUsuario");
     	Vector<String> listado = (Vector<String>)request.getSession().getAttribute("listado");
+    	Integer contador= (Integer) getServletContext().getAttribute("contador");
     	
     	if (listado == null){listado = new Vector();}
-    	
-		if (nombre != null ){
-			
-			out.println("<br>Hola "+nombre+"<br>");
-			listado.addElement(nombre);
-		}
+		if (nombre != null ){out.println("<br>Hola "+nombre+"<br>");listado.addElement(nombre);}
+		if ( contador == null ){contador = new Integer(1);}
     	
     	request.getSession().setAttribute("listado",listado);
-    	
+    	getServletContext().setAttribute("contador",new Integer(contador.intValue()+1));
     	
     	response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
@@ -59,8 +56,9 @@ public class HolaMundoServlet extends HttpServlet {
 		for ( int i = 0 ; i < listado.size() ; i++ ){
 			out.println("<br>"+(String)listado.elementAt(i));
 		}
+		out.println("<br><br>" + contador +" visitas<br>");
 		out.println("<a href=\"index.html\">volver</a>");
-
+		
 		
 		out.println("</BODY></HTML>");
 		
