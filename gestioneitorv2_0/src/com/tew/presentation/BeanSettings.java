37e01d086e5b854fb72a8f3a25fb2fa5b2e57ddd
JacomeCostales <uo263748@uniovi.es>
2020-10-06 17:32:09 +0200

    TEW-2021-102-4.6-Inyección Dependencia Bean Alumnos y Bean Settings

--- gestioneitorv2_0/src/com/tew/presentation/BeanSettings.java
+++ gestioneitorv2_0/src/com/tew/presentation/BeanSettings.java
@@ -1,6 +1,9 @@
 package com.tew.presentation;
 import java.io.Serializable;
 import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.faces.bean.*;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
@@ -17,14 +20,8 @@ public class BeanSettings implements Serializable{
 	 @ManagedProperty(value="#{alumno}")
 	  private BeanAlumno alumno;
 	 
-	 public void setAlumno(Alumno alumno) {
-		 this.alumno = (BeanAlumno) alumno;
-		}
-		public BeanAlumno getAlumno(){
-		 return this.alumno;
-		}
-	 
-	 
+	 public void setAlumno(BeanAlumno alumno) {this.alumno =alumno;	}
+	public BeanAlumno getAlumno(){ return this.alumno;	}
 	 
 	 public Locale getLocale() { /*Habria que cambiar algo de código para coger locale
 	del navegador la primera vez que se accede a getLocale(), de momento el idioma de
@@ -33,10 +30,54 @@ public class BeanSettings implements Serializable{
 	 }
 	 public void setSpanish(ActionEvent event) {
 	 locale = SPANISH;
-	 FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	 try {
+		 FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		 if (alumno != null)
+		 if (alumno.getId()== null) //valores por defecto del alumno, si no NO inicializar
+		 alumno.iniciaAlumno(null);
+		 } catch (Exception ex){
+		 ex.printStackTrace();
+		 }
 	 }
 	 public void setEnglish(ActionEvent event) {
 	 locale = ENGLISH;
-	 FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	 try {
+		 FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		 if (alumno != null)
+		 if (alumno.getId()== null) //valores por defecto del alumno, si no NO inicializar
+		 alumno.iniciaAlumno(null);
+		 } catch (Exception ex){
+		 ex.printStackTrace();
+		 }
 	}
+	 
+	//Se inicia correctamente el Managed Bean inyectado si JSF lo hubiera creado
+	//y en caso contrario se crea.
+	//(hay que tener en cuenta que es un Bean de sesión)
+	//Se usa @PostConstruct, ya que en el contructor no se sabe todavía si
+	//el MBean ya estaba construido y en @PostConstruct SI.
+	@PostConstruct
+	public void init() {
+	 System.out.println("BeanSettings - PostConstruct");
+	 //Buscamos el alumno en la sesión. Esto es un patrón factoría claramente.
+	 alumno =
+	 (BeanAlumno)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(new
+	String("alumno"));
+	 //si no existe lo creamos e inicializamos
+	 if (alumno == null) {
+	 System.out.println("BeanSettings - No existia");
+	 alumno = new BeanAlumno();
+	 FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "alumno",
+	alumno);
+	 }
+	}
+	//Es sólo a modo de traza.
+	@PreDestroy
+	public void end(){
+		 System.out.println("BeanSettings - PreDestroy");
+	}
+	
+	
+	 
+	 
 }
